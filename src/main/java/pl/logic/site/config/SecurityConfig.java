1fdd01318b67d5333a3628fe7787f1eab66532e3
Jakub Kowalik <j.kowalik0928@gmail.com>
2024-06-04 21:20:45 +0200

    redirec to react home page

--- src/main/java/pl/logic/site/config/SecurityConfig.java
+++ src/main/java/pl/logic/site/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package pl.logic.site.config;
 
 import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,8 +13,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -30,11 +42,51 @@ public class SecurityConfig {
                                 .requestMatchers(WHITE_LIST_URL).permitAll()
                                 .anyRequest().authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpoint ->
+                                userInfoEndpoint.oidcUserService(this.oidcUserService())
+                        )
+                        .successHandler(new CustomAuthenticationSuccessHandler())
+                )
+
 //                .formLogin(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
+
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        OidcUserService delegate = new OidcUserService();
+
+        return (userRequest) -> {
+            OidcUser oidcUser = delegate.loadUser(userRequest);
+
+            Map<String, Object> attributes = oidcUser.getAttributes();
+            String email = (String) attributes.get("email");
+            String firstName = (String) attributes.get("given_name");
+            String lastName = (String) attributes.get("family_name");
+            return oidcUser;
+        };
+    }
 }
+
+@Component
+class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                         Authentication authentication) throws IOException {
+        OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
+        String email = oidcUser.getEmail();
+        String firstName = oidcUser.getGivenName();
+        String lastName = oidcUser.getFamilyName();
+
+        // Build the redirect URL with user information as query parameters
+        String targetUrl = String.format("http://localhost:3000/home?email=%s&firstName=%s&lastName=%s",
+                email, firstName, lastName);
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+}
+
